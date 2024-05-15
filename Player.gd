5e68c8a40ce592f80fcb7ed74aa5f05c900b6c24
TDTank <tidusdurbin@gmail.com>
2024-05-14 19:23:41 -0700

    Fixed player movement

--- Player.gd
+++ Player.gd
@@ -2,15 +2,16 @@ extends CharacterBody2D
 
 var health = 100.0
 
-
 signal health_depleted
 
 func _physics_process(delta):
 	var direction = Input.get_vector("MoveLeft","MoveRight" ,"MoveUp" ,"MoveDown")
 	velocity = direction * 400
 	move_and_slide()
+	if velocity.length() != 0:
+		%Raft.rotation = velocity.angle() - (PI/2)
 
-	const DAMAGE_RATE = 50.0
+	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
