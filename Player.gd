398cc305065e372810825ac48809f22313b809e0
N1xon4Ever <sam.diode2@gmail.com>
2024-05-14 22:01:01 -0500

    added boat turning

--- Player.gd
+++ Player.gd
@@ -1,15 +1,51 @@
 extends CharacterBody2D
 
-var health = 100.0
+var health := 100.0
+var direction := 0.0
+var speed := 0.0
+
+var ang_accel :=.008
+var turn_max :=.05
+var accel := 500.0
+var speed_max := 200.0
+
 
 signal health_depleted
 
 func _physics_process(delta):
-	var direction = Input.get_vector("MoveLeft","MoveRight" ,"MoveUp" ,"MoveDown")
-	velocity = direction * 400
+	#SPEED INPUT
+	if Input.is_action_pressed("MoveUp"):
+		speed += accel*delta
+	elif Input.is_action_pressed("MoveDown"):
+		speed -= accel*delta
+	elif speed < 0:
+		speed += accel*delta
+		if speed >0:
+			speed = 0
+	elif speed > 0:
+		speed -= accel*delta
+		if speed < 0:
+			speed = 0
+	#SPEED CLAMP
+	if speed > speed_max:
+		speed = speed_max
+	elif speed < -speed_max:
+		speed = -speed_max
+	var dir_input := 0.0
+	if Input.is_action_pressed("MoveLeft"):
+		dir_input = -1
+	if Input.is_action_pressed("MoveRight"):
+		dir_input += 1
+	
+	direction +=clampf(speed*dir_input*delta*ang_accel,-turn_max,turn_max)
+	
+	velocity = Vector2.from_angle(direction)*speed
 	move_and_slide()
 	if velocity.length() != 0:
-		%Raft.rotation = velocity.angle() - (PI/2)
+		if speed >0:
+			%Raft.rotation = velocity.angle() - (PI/2)
+		else:
+			%Raft.rotation = velocity.angle() + (PI/2)
 
 	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
