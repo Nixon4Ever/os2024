327751ee3003c067f0d725983f3a2c0c983313b7
TDTank <tidusdurbin@gmail.com>
2024-05-18 16:19:42 -0700

    fixed exp crashing

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -7,9 +7,6 @@ var experience = 1
 @onready var player = $"../Player"
 @onready var loot_base = get_tree().get_first_node_in_group("loot")
 
-
-var exp_gem = load("res://scenes/experince_orb.tscn")
-
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * movement_speed
@@ -27,7 +24,7 @@ func death():
 	var new_gem = exp_gem.instantiate()
 	new_gem.global_position = global_position
 	new_gem.experience = experience
-	loot_base.add_child(new_gem)
+	loot_base.call_deferred("add_child",new_gem)
 	queue_free()
 	
 
