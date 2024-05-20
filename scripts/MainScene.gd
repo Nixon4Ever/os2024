4a67ae621dc8231f9f6bbc567706d0510ee05d3f
N1xon4Ever <sam.diode2@gmail.com>
2024-05-19 21:02:46 -0500

    added main menu

---
+++ scripts/MainScene.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var gameScene = preload("res://scenes/Main2D.tscn")
+@onready var menu = $MainMenu
+var gameObj = null
+
+var in_game := false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func end_game():
+	if in_game:
+		in_game=false
+		menu.visible=true
+		gameObj.queue_free()
+
+func _on_play_button_button_down():
+	if !in_game:
+		in_game=true
+		menu.visible=false
+		gameObj = gameScene.instantiate()
+		add_child(gameObj)
+
+
+func _on_quit_button_button_down():
+	get_tree().quit()
