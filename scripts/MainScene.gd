3c5783e5971a578a209a56c2ae16595b0dd6d718
N1xon4Ever <sam.diode2@gmail.com>
2024-05-19 21:34:27 -0500

    main menu art

--- scripts/MainScene.gd
+++ scripts/MainScene.gd
@@ -2,6 +2,7 @@ extends Node
 
 var gameScene = preload("res://scenes/Main2D.tscn")
 @onready var menu = $MainMenu
+@onready var timer = $CanvasLayer/Timer
 var gameObj = null
 
 var in_game := false
@@ -13,12 +14,14 @@ func end_game():
 	if in_game:
 		in_game=false
 		menu.visible=true
+		timer.visible = false
 		gameObj.queue_free()
 
 func _on_play_button_button_down():
 	if !in_game:
 		in_game=true
 		menu.visible=false
+		timer.visible = true
 		gameObj = gameScene.instantiate()
 		add_child(gameObj)
 
