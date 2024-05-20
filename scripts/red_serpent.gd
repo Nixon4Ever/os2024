1c132faf2b13b3850d5c9c2c0f5cf62f3d322e96
TDTank <tidusdurbin@gmail.com>
2024-05-19 20:26:18 -0700

    boss spawns

---
+++ scripts/red_serpent.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+var movement_speed = 250
+var health = 20
+var experience = 2
+var damage = 7
+
+@onready var player = $"../Player"
+@onready var loot_base = get_tree().get_first_node_in_group("loot")
+@onready var ap = $AnimationPlayer
+@onready var hit = get_tree().root.get_child(0).get_node("enemyhit")
+@onready var enemydeath = get_tree().root.get_child(0).get_node("enemydeath")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
+
+func take_damage(damage):
+	health -= damage
+	hit.play()
+	if health <= 0:
+		death()
+
+func death():
+	print("DEATH")
+	movement_speed = 0
+	$CollisionShape2D.queue_free()
+	ap.play("RedSerpentDeath")
+
+func _on_animation_player_animation_finished(RedSerpentDeath):
+	var exp_gem = load("res://scenes/experince_orb.tscn")
+	var new_gem = exp_gem.instantiate()
+	new_gem.global_position = global_position
+	new_gem.experience = experience
+	loot_base.call_deferred("add_child",new_gem)
+	queue_free()
